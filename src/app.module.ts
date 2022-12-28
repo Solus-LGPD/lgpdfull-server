76d13259e050acb42ae54d2b5111a41869062ec7
celso <celso.botelho14@gmail.com>
2022-12-28 16:59:09 -0300

    feat:Validação do JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 import { UserModule } from './modules/user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [UserModule, AuthModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,     {
+    provide: APP_GUARD,
+    useClass: JwtAuthGuard,
+  },],
 })
 export class AppModule {}
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from '../user/user.module';
-import { UserService } from '../user/user.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt-strategy';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
@@ -13,6 +13,6 @@ import { LocalStrategy } from './strategies/local.strategy';
     signOptions: { expiresIn: '30d' }
   })],
   controllers: [AuthController],
-  providers: [AuthService, LocalStrategy]
+  providers: [AuthService, LocalStrategy, JwtStrategy]
 })
 export class AuthModule {}
