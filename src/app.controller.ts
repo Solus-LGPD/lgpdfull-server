5e5b3aa87fdfa0a920cb4edc8b5be05609ab9954
celso <celso.botelho14@gmail.com>
2022-12-28 17:12:00 -0300

    feat:Rota Get(me) para usuários

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-//import { CurrentUser } from './auth/decorators/current-user.decorator';
-//import { User } from './user/entities/user.entity';
+import { IsPublic } from 'src/modules/auth/decorators/is-public.decorator';
+import { CurrentUser } from 'src/modules/auth/decorators/current-user.decorator';
+import { User } from 'src/modules/user/entities/user.entity';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
   @Get()
   getHello(){
     return this.appService.getHello();
   }
 
-  /*@Get('/me')
+  @Get('/me')
   getMe(@CurrentUser() currentUser: User) {
     return currentUser;
-  }*/
+  }
 }
