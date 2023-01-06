9d296feb7e0452682dc73351236f7527560e59af
celso <celso.botelho14@gmail.com>
2023-01-06 17:34:30 -0300

    feat:Add Helmet protection

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.use(helmet());
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
